d8aed0c44edd5778b815b15a79ac0141c92d9e1b
DragonHuan <961699913@qq.com>
2021-03-08 17:03:54 +0800

    init

---
+++ eureka-server/src/main/java/com/dragon/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.dragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Dragon
+ */
+@SpringBootApplication
+public class EurekaApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
