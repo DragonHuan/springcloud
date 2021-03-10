f414fef4de659751075d9bee685792ad875de133
DragonHuan <961699913@qq.com>
2021-03-11 00:48:59 +0800

    Eureka服务及客户

--- eureka/eureka-client/src/main/java/com/dragon/ClientApplication.java
+++ eureka/eureka-client/src/main/java/com/dragon/ClientApplication.java
@@ -2,17 +2,16 @@ package com.dragon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by Dragon
  */
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaApplication {
-
+@EnableEurekaClient
+public class ClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class,args);
+        SpringApplication.run(ClientApplication.class,args);
     }
 }
---
+++ eureka/eureka-server/src/main/java/com/dragon/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.dragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Dragon
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
