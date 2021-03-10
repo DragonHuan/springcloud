55541f7cb429b4659670d917caa224d1bf48f3fb
DragonHuan <961699913@qq.com>
2021-03-10 10:13:05 +0800

    更新Eureka启动

--- eureka-server/src/main/java/com/dragon/EurekaApplication.java
+++ eureka-server/src/main/java/com/dragon/EurekaApplication.java
@@ -2,11 +2,13 @@ package com.dragon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Created by Dragon
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
 
